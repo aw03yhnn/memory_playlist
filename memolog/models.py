50cd0cdd767fa0315fa03bbcfe81385681219e73
aw03yhnn <nonkschannn@gmail.com>
2025-06-24 03:50:17 +0900

    Add Memory model and initial migration

--- memolog/models.py
+++ memolog/models.py
@@ -1,1 +1,27 @@
 from django.db import models
+
+# 記憶とSpotifyの曲を1セットで保存するモデル
+class Memory(models.Model):
+    # 記憶のタイトル（例：「高校の帰り道」）
+    title = models.CharField(max_length=100, verbose_name='タイトル')
+    # その記憶の詳細な内容や思い出
+    description = models.TextField(verbose_name='記憶の内容')
+    # その記憶に関連する曲のタイトル
+    song_title = models.CharField(max_length=200, verbose_name='曲名')
+    # 曲のアーティスト名
+    artist_name = models.CharField(max_length=200, verbose_name='アーティスト名')
+    # Spotifyの曲へのリンク
+    spotify_url = models.URLField(verbose_name='SpotifyのURL')
+    # 投稿された日時（自動で現在時刻が設定される）
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='投稿日時')
+    
+    # 管理画面での表示設定
+    class Meta:
+        verbose_name = '記憶'
+        verbose_name_plural = '記憶'
+        # 新しい投稿が上に表示されるように並び順を設定
+        ordering = ['-created_at']
+    
+    # 管理画面でタイトルが表示されるように設定
+    def __str__(self):
+        return self.title
