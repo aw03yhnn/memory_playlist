d7f20424e1e447e4f21bec5ffcca23030f8f7b2a
aw03yhnn <nonkschannn@gmail.com>
2025-06-26 20:44:25 +0900

    Add memory_create view for form handling

--- memolog/views.py
+++ memolog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
 from .models import Memory
+from .forms import MemoryForm
 
 # Create your views here.
 
@@ -14,3 +16,19 @@ def memory_detail(request, pk):
     # 指定されたIDの記憶を取得（存在しない場合は404エラー）
     memory = get_object_or_404(Memory, pk=pk)
     return render(request, 'memolog/memory_detail.html', {'memory': memory})
+
+# 新しい記憶を投稿するビュー
+def memory_create(request):
+    if request.method == 'POST':
+        # フォームデータが送信された場合
+        form = MemoryForm(request.POST)
+        if form.is_valid():
+            # フォームデータを保存
+            memory = form.save()
+            # 保存後は詳細ページにリダイレクト
+            return redirect('memolog:memory_detail', pk=memory.pk)
+    else:
+        # GETリクエストの場合は空のフォームを表示
+        form = MemoryForm()
+    
+    return render(request, 'memolog/memory_form.html', {'form': form})
